d4124ef865849db5b63a5c3e0ed5b0d93d637f28
Aayush Grover <aayushgrover101@gmail.com>
2025-08-22 19:33:31 -0400

    Setup folder structure

---
+++ src/styles/index.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import type { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  colors: {
+    text: {},
+    primary: {
+      blue: "#0088FF",
+    },
+    secondary: {},
+    shadow: {},
+    state: {
+      error: "",
+      success: "",
+      infomative: "",
+      warning: "",
+    },
+  },
+};
